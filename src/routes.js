e1c7e77839036dcb230f35aa49b1f834ba66b2da
Vinicius Miguel <viniciuszim@hotmail.com>
2019-07-17 18:19:02 -0300

    adicionado exception handling

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,28 @@
 const express = require('express')
+const validate = require('express-validation')
+const handle = require('express-async-handler')
 
 const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 // Session's Routes
-routes.post('/sessions', controllers.SessionController.store)
+routes.post('/sessions', validate(validators.Session), handle(controllers.SessionController.store))
 
 // User's Routes
-routes.post('/users', controllers.UserController.store)
+routes.post('/users', validate(validators.User), handle(controllers.UserController.store))
 
 // Force all routes below to use authentication
 routes.use(authMiddleware)
 
 // Tool's Routes
-routes.get('/tools', controllers.ToolController.index)
-routes.get('/tools/:id', controllers.ToolController.show)
-routes.post('/tools', controllers.ToolController.store)
-routes.put('/tools/:id', controllers.ToolController.update)
-routes.delete('/tools/:id', controllers.ToolController.destroy)
+routes.get('/tools', handle(controllers.ToolController.index))
+routes.get('/tools/:id', handle(controllers.ToolController.show))
+routes.post('/tools', validate(validators.Tool), handle(controllers.ToolController.store))
+routes.put('/tools/:id', validate(validators.Tool), handle(controllers.ToolController.update))
+routes.delete('/tools/:id', handle(controllers.ToolController.destroy))
 
 module.exports = routes
